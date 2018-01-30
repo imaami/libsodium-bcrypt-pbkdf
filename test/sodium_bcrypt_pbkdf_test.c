273e274f7a91b9cd475ee853644ff84a0e38ccda
Juuso Alasuutari <juuso.alasuutari@gmail.com>
2018-01-30 18:46:47 +0200

    Add aes256-ctr decryption code to test program.

--- test/sodium_bcrypt_pbkdf_test.c
+++ test/sodium_bcrypt_pbkdf_test.c
@@ -17,14 +17,29 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdio.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <inttypes.h>
 #include <sodium.h>
 
+#include <openssl/conf.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+
 #include "sodium_bcrypt_pbkdf.h"
+#include "hexdump.h"
+
+__attribute__((always_inline))
+static inline int decrypt( uint8_t *ciphertext, size_t ciphertext_len,
+                           uint8_t *key, uint8_t *iv, uint8_t *plaintext );
 
 int main( int    argc,
           char **argv )
@@ -34,27 +49,99 @@ int main( int    argc,
 	}
 
 	uint8_t salt[16];
-	uint8_t key[48];
+	uint8_t hash[48];
 
-	randombytes_buf(salt, sizeof(salt));
+	memset(&salt[0], 0, sizeof(salt)); // Obviously not a decent salt
+	memset(&hash[0], 0, sizeof(hash));
 
-	fputs("salt: ", stdout);
-	for (size_t i = 0; i < sizeof(salt); ++i) {
-		printf("%02x", salt[i]);
-	}
-	puts("");
 
 	int r = sodium_bcrypt_pbkdf(argv[1], strlen(argv[1]),
 	                            salt, sizeof(salt),
-	                            key, sizeof(key), 16);
+	                            hash, sizeof(hash), 16);
 
-	fputs("key:  ", stdout);
-	if (r == 0) {
-		for (size_t i = 0; i < sizeof(key); ++i) {
-			printf("%02x", key[i]);
-		}
+	uint8_t *key = &hash[0], *iv = &hash[32];
+
+	// Obviously not actual ciphertext (but who knows, right?)
+	uint8_t ciphertext[] = {
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	};
+	uint8_t plaintext[BUFSIZ] = { '\0' };
+
+	puts("+------+----------------------------------------------------+----------------+");
+	hexdump((const uint8_t *)salt, sizeof(salt), "salt", 4);
+	puts("+------+----------------------------------------------------+----------------+");
+	hexdump((const uint8_t *)key, 32, "key ", 4);
+	puts("+------+----------------------------------------------------+----------------+");
+	hexdump((const uint8_t *)iv, 16, "iv  ", 4);
+	puts("+------+----------------------------------------------------+----------------+");
+	hexdump((const uint8_t *)ciphertext, sizeof(ciphertext), "enc ", 4);
+
+	r = decrypt(ciphertext, sizeof(ciphertext), key, iv, plaintext);
+	if (r > 0) {
+		puts("+------+----------------------------------------------------+----------------+");
+		hexdump((const uint8_t *)plaintext, (size_t)r, "dec ", 4);
 	}
-	puts("");
 
-	return r;
+	puts("+------+----------------------------------------------------+----------------+");
+
+	return (r == -1) ? EXIT_FAILURE : EXIT_SUCCESS;
 }
+
+__attribute__((always_inline))
+static inline int decrypt( uint8_t *ciphertext, size_t ciphertext_len,
+                           uint8_t *key, uint8_t *iv, uint8_t *plaintext )
+{
+	EVP_CIPHER_CTX *ctx = NULL;
+	int len = 0, plaintext_len = 0;
+
+	if (!(ctx = EVP_CIPHER_CTX_new())) {
+		ERR_print_errors_fp(stderr);
+		return -1;
+	}
+
+	if (EVP_DecryptInit_ex(ctx, EVP_aes_256_ctr(), NULL, key, iv) != 1) {
+		ERR_print_errors_fp(stderr);
+		return -1;
+	}
+
+	if (EVP_DecryptUpdate(ctx, plaintext, &len,
+	                      ciphertext, (int)ciphertext_len) != 1) {
+		ERR_print_errors_fp(stderr);
+		return -1;
+	}
+
+	plaintext_len = len;
+
+	if (EVP_DecryptFinal_ex(ctx, plaintext + len, &len) != 1) {
+		ERR_print_errors_fp(stderr);
+		return -1;
+	}
+
+	plaintext_len += len;
+
+	/* Clean up */
+	EVP_CIPHER_CTX_free(ctx);
+
+	return plaintext_len;
+}
+
+#ifdef __cplusplus
+}
+#endif
